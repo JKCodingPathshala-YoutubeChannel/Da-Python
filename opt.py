01f668e89f32b01bb2a90102fe56c614e7832aa0
Jayesh Kande <jayeshkande55@gmail.com>
2026-06-20 11:07:49 +0530

    Add files via upload

---
+++ opt.py
@@ -0,0 +1,36 @@
+# a=2
+# b=3
+
+# print("add =",a+b)
+# print("sub =",a-b)
+# print("mul =",a*b)
+# print("Div =",a/b)
+# print("fdiv=",a//b)
+# print("power=",a**b)
+# print("rem=",a%b)
+
+
+x=10
+y="10"
+
+# print(x==y)
+# print(type(x==y))
+# print(int(x!=y))
+
+# print(x>y)
+# print(x<y)
+
+# print(x>=y)
+# print(x<=y)
+
+
+
+p=10
+q=20
+
+print(p!=q and p<q)
+
+print(p==q or p>q)
+
+print(not(p!=q and p<q))
+
