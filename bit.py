343721e9ab6af2c9017706568d1d944cf5797897
Jayesh Kande <jayeshkande55@gmail.com>
2026-06-22 10:48:09 +0530

    Add files via upload

---
+++ bit.py
@@ -0,0 +1,35 @@
+# a=5
+# b=1
+
+# print(a&b)
+# print(a|b)
+# print(~a)
+# print(a^b)
+# print(a<<b)
+# print(a>>b)
+
+# l=[1,2,3]
+# print(3 not in l)
+
+# x=[1,2,3]
+# y=[1,2,3,4]
+# z=x
+# print(x is z)
+# print(x is not y)
+# print(x==y)
+
+# temp=a
+# a=b
+# b=temp
+
+a=5
+b=1
+print("a=",a,"b=",b)
+
+a,b=b,a
+
+# a=a^b
+# b=a^b
+# a=a^b
+
+print("a=",a,"b=",b)
